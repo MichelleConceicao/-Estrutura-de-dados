c28ca9d88cd5022317aa42f64a7dfa0067b1e635
MichelleConceição <liliandlentretenimento@gmail.com>
2022-06-09 13:56:27 -0300

    Desafio finalizado

---
+++ script.js
@@ -0,0 +1,28 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let nome ="";
+console.log(nome);
+
+let numero = 5; 
+let ano = 2022;
+const resultado = numero - ano;
+console.log(numero - ano);
+
+let mensagem =" Estou mexendo no console.log";
+console.log(mensagem);
+
+const quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
